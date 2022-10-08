1ca33c8f742b51a7d8022814dcb4c32e704cee67
Nabih Uzcategui <nabihuzcateguip@gmail.com>
2022-10-07 23:42:40 -0300

    html and CSS update / javascript file creation

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+const formulario = document.querySelector('#formulario');
+const buttonMailTo = document.querySelector('#send-mail');
+
+//Crear el evento 
+formulario.addEventListener('submit', handleSubmit);
+
+
+function handleSubmit(e) {
+
+    e.preventDefault();
+
+    const form = new FormData(this);
+
+    buttonMailTo.setAttribute('href', `mailto:nabihuzcateguip@gmail.com?subject=
+        Mensaje enviado desde mi sitio web por: ${form.get("fullname")}
+        Correo: ${form.get("email")}
+        &body=
+        Nombre: ${form.get("fullname")}
+        Correo: ${form.get("email")}
+        Teléfono: ${form.get("phone")}
+        Asunto: ${form.get("affair")}
+        mensaje: ${form.get("message")}`
+    );
+
+    buttonMailTo.click();
+};
