ce97dadc05cb869f4c656d53c26b383fff14792e
Nabih Uzcategui <nabihuzcateguip@gmail.com>
2022-10-27 16:59:19 -0300

    Actualización HTML - CSS / delete contacto.html

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
+//** Formulario - Nabih U.- */
+const formulario = document.querySelector("#formulario");
+const buttonMailTo = document.querySelector("#send-mail");
 
-
-const formulario = document.querySelector('#formulario');
-const buttonMailTo = document.querySelector('#send-mail');
-
-//Crear el evento 
-formulario.addEventListener('submit', handleSubmit);
-
+//Crear el evento
+formulario.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
+  e.preventDefault();
 
-    e.preventDefault();
+  const form = new FormData(this);
 
-    const form = new FormData(this);
-
-    buttonMailTo.setAttribute('href', `mailto:nabihuzcateguip@gmail.com?subject=
+  buttonMailTo.setAttribute(
+    "href",
+    `mailto:nabihuzcateguip@gmail.com?subject=
         Mensaje enviado desde mi sitio web por: ${form.get("fullname")}
         Correo: ${form.get("email")}
         &body=
@@ -22,7 +21,19 @@ function handleSubmit(e) {
         Teléfono: ${form.get("phone")}
         Asunto: ${form.get("affair")}
         mensaje: ${form.get("message")}`
-    );
+  );
 
-    buttonMailTo.click();
-};
+  buttonMailTo.click();
+}
+
+
+//** API leaflet - Mapa */
+const tilesProvider = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+let myMap = L.map("myMap").setView([-34.60276203812859, -58.403056090797016], 11);
+
+L.tileLayer(tilesProvider, {
+  maxZoom: 20,
+}).addTo(myMap);
+
+let marker = L.marker([-34.60276203812859, -58.403056090797016]).addTo(myMap);
